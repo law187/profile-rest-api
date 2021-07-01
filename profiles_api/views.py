dab7bbf67be80997b3a495b212f43ca0a65ee85b
moe <moe@gmail.com>
2021-07-02 02:06:13 +0800

    created api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """docstring for ."""
+
+    def get(self, request, format=None):
+        an_apiview = ['get, post ,patch ,update , delete : this is  lsit of apiviews' ]
+        return Response({'an_apiview':an_apiview})
