c3732e521b3cda9e1f128004156812362227b11c
moe <moe@gmail.com>
2021-07-06 05:24:49 +0800

    addes viewset to profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
 from rest_framework import status
+
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -42,3 +43,37 @@ class HelloApiView(APIView):
     def delete(self,request, PK=None):
 
         return Response({'method' : 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+        serializer_class = serializers.HelloSerializer
+
+
+        def list(self, request):
+
+            a_viewsets = ['list, create , retrive, update, partial_update']
+
+            return Response({'message':'hello', 'a_viewsets':a_viewsets})
+
+
+        def create(self, request):
+              serializer =self.serializer_class(data=request.data)
+              if serializer.is_valid():
+                    name = serializer.validated_data.get('name')
+                    message = f'hello{name}'
+                    return Response({'message':message})
+              else:
+                 return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        def retrive(slef, request, pk=None):
+            return Response({'method_http':'GET'})
+
+        def update(self, request, pk=None):
+            return Response({'method_http':'PUT'})
+
+        def partial_update(self, request, pk=None):
+            return Response({'method_http':'PATCH'})
+
+        def destroy(self, request, pk=None):
+            return Response({'method_http':'DELETE'})
